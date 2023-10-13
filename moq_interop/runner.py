ce0611dbc71a66a9df8838b53cfbcc9ee6d0226d
Sarah Grant <sarah.ietf@gmail.com>
2023-10-13 14:52:49 +0000

    Putting runner in class

--- moq_interop/runner.py
+++ moq_interop/runner.py
@@ -1,12 +1,21 @@
+import json
 import logging
 from argparse import ArgumentParser
 
 
-def main() -> None:
-    logging.info("Runner started")
+class Runner:
 
-    parser = ArgumentParser(prog="moq-interop-runner")
-    parser.add_argument("-c", "--config", type=str, required=True,
-                        help="Configuration file")
-    args = parser.parse_args()
+    @staticmethod
+    def main() -> None:
+        logging.basicConfig(level=logging.INFO)
+        logging.info("Runner started")
 
+        parser = ArgumentParser(prog="moq-interop-runner")
+        parser.add_argument("-c", "--config", type=str, required=False,
+                            default="config/implementations.json",
+                            help="Configuration file")
+        args = parser.parse_args()
+
+        # Generate each permutation to test
+        with open(args.config) as imp_json:
+            implementations = json.load(imp_json)
