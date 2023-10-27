786b18fb237c3d44b7efe0fd1025e4169ab6ab59
Sarah Grant <sarah.ietf@gmail.com>
2023-10-27 16:29:39 +0000

    Updating to transport-01

--- moq_interop/transport.py
+++ moq_interop/transport.py
@@ -14,19 +14,26 @@ class MoqTransport:
 
 
 class MoqMessageType(IntEnum):
-    OBJECT = 0x0
-    SETUP = 0x1
-    SUBSCRIBE_REQUEST = 0x3
+    OBJECT_WITH_PAYLOAD = 0x0
+    OBJECT_WITHOUT_PAYLOAD = 0x2
+    SUBSCRIBE = 0x3
     SUBSCRIBE_OK = 0x4
     SUBSCRIBE_ERROR = 0x5
     ANNOUNCE = 0x6
     ANNOUNCE_OK = 0x7
     ANNOUNCE_ERROR = 0x8
+    UNANNOUNCE = 0x9
+    UNSUBSCRIBE = 0xA
+    SUBSCRIBE_FIN = 0xB
+    SUBSCRIBE_RST = 0xC
     GOAWAY = 0x10
+    CLIENT_SETUP = 0x40
+    SERVER_SETUP = 0x41
 
 
 class MoqError(IntEnum):
-    TERMINATED = 0x0
+    NO_ERROR = 0x0
     GENERIC = 0x1
     UNAUTHORIZED = 0x2
+    PROTOCOL_VIOLATION = 0x3
     GOAWAY = 0x10
